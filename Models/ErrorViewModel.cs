5e7b514b349998fbba8992300df8d9ffb81a6d74
xalonso <lgv.alonso@gmail.com>
2019-06-28 02:10:59 +0000

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BestBikeApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
